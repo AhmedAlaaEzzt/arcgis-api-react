561a0a76edc75a74f8773345e3137faf8d292e6a
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2022-07-15 20:46:53 +0200

    00002 - prepare the demo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import './App.css';
+import "./App.css";
+import { useRef, useEffect } from "react";
+
+import esriConfig from "@arcgis/core/config";
+import Map from "@arcgis/core/Map";
+import MapView from "@arcgis/core/views/MapView";
 
 function App() {
-  return (
-    <div className="">
-      
-    </div>
-  );
+  const mapRef = useRef();
+ 
+  useEffect(() => {
+    esriConfig.apiKey = "AAPKa303a70299c5477fbb4546010ac5eeabqxdUF8v_c4svuKvFI0XEbxPXbkhalk6YVekiJPQfaplQmO7DXXh__X270OOTrutL";
+
+    const map = new Map({
+      basemap: "osm-streets",
+    });
+
+
+    const view = new MapView({
+      container: mapRef.current,
+      map: map,
+      center: [-118, 34],
+      zoom: 8,
+    });
+
+  }, []);
+
+  return <div className="viewDiv" ref={mapRef} />;
 }
 
 export default App;
